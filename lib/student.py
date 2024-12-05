8b3cb0ddde2d095d189fa1ba8750f98198410368
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-05 15:11:20 +0300

    Implement Teacher class with inheritance from User, adding knowledge list and teach method

--- lib/student.py
+++ lib/student.py
@@ -3,7 +3,7 @@
 from user import User
 
 class Student(User):
-      def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)  # Initialize parent class (User)
         self.knowledge = []  # Initialize the knowledge attribute as an empty list
 
