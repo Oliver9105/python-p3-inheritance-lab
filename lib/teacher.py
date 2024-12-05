d1f91cd9fc8c5eed84cab11bb2c0ed9b2db52de6
Oliver cheruiyot <olivercher1000@gmail.com>
2024-12-05 14:51:21 +0300

    Implement Teacher class with inheritance from User, adding knowledge list and teach method

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,22 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        # Initialize the User class
+        super().__init__(first_name, last_name)
+        
+        # Add a list of knowledge items
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
+    
+
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
